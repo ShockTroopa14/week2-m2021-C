871bd9e0d16e51fd62c57973a4a92fa3d60b26a7
ShockTroopa14 <52359305+ShockTroopa14@users.noreply.github.com>
2021-05-17 16:20:30 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World2');
+});
+let myVariable = server.httplock;
+console.log(myVariable);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+//# sourceMappingURL=index.js.map
